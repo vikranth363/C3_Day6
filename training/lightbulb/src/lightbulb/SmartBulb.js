544145bb26219818c2e8e615141d6aa341ccff2c
Charles QUELENNEC <quelennec.charles@gmail.com>
2018-04-09 14:42:10 -0700

    shortest

--- training/lightbulb/src/lightbulb/SmartBulb.js
+++ training/lightbulb/src/lightbulb/SmartBulb.js
@@ -17,45 +17,59 @@ function lifeSpanInYears(bulbId){
   startTime = bulb.startDate;
   defectFilter = "status == 1 && lumens == 0 && parent.id == '" + 'SBMS_serialNo_' + bulb.id + "'";
   defectDatum = SmartBulbMeasurement.fetch({filter:defectFilter});
-  defectTime = defectDatum.objs[0].end;
-  lifespan = defectTime - startTime;
   conversionFactor = 1000*60*60*24*365;
+  if (defectDatum.objs.length == 0) {
+    defectTime = 50 * conversionFactor;
+  } else {
+
+    if (defectDatum.objs[0].end) {
+      defectTime = defectDatum.objs[0].end;
+    } else {
+      defectTime = 50 * conversionFactor;
+    }
+    
+  }
+
+  lifespan = defectTime - startTime;
   lifeSpanInYears = lifespan / conversionFactor;
   return lifeSpanInYears;
 }
 
 function shortestLifeSpanBulb() {
 
-  // Get all smartbulbs
+  // Get all smartbulbs.
   var allSmartBulbs = SmartBulb.fetch();
 
   if(allSmartBulbs.objs.length == 0) {
      return -1;
   }
 
-  // Initialize the shortest lifespan
+  // Initialize the shortest lifespan.
   var id = allSmartBulbs.objs[0].id;
 
   var shortestLiveSpanBulb = SmartBulb.lifeSpanInYears(id);
   
+  // Intialize the id of the bulb with the shortest lifespan.
   var result = id;
 
   // foreach smartbulb ...
-  allSmartBulbs.objs.each(function(smartbulb){
+  allSmartBulbs.objs.each(function(smartbulb) {
 
-    // Get the lifespan of the current smartbul
+    // ... get the lifespan of the current smartbulb ...
     var liveSpanBulb = SmartBulb.lifeSpanInYears(smartbulb.id);
+    
+    // ... compare and ...
+    if (liveSpanBulb < shortestLiveSpanBulb) {
 
-    if(liveSpanBulb){
+      // ... shortest livespan ...
+      shortestLiveSpanBulb = liveSpanBulb;
 
-      // Compare and set the new livespan
-      if(liveSpanBulb < shortestLiveSpanBulb){
-        result = smartbulb.id;
-      }
-
-    } 
+      // ... set id of the new shortest livespan.
+      result = smartbulb.id;
+    }
     
   });
 
   return result;
+
 }
