c339ecb0ea52685b3bf08b4dd3193f497339659d
Charles QUELENNEC <quelennec.charles@gmail.com>
2018-04-10 14:10:08 -0700

    defective

--- training/lightbulb/src/analytics/SmartBulbDefective.js
+++ training/lightbulb/src/analytics/SmartBulbDefective.js
@@ -1,18 +1,16 @@
 var log = C3.logger("SmartBulbLongLifeAlert");
 
-var DURATION_THRESHOLD = 400;
-
 function process(input){
 
-    var data = input.bulbLife.data(),
-        dates = input.bulbLife.dates();
+    var data = input.bulbDefective.data(),
+        dates = input.bulbDefective.dates();
 
     for (var i = 0; i < data.length; i++) {
 
-        if (data.at(i) > DURATION_THRESHOLD) {
+        if (data.at(i)) {
             return SmartBulbEvent.make({
                 smartBulb: input.source,
-                eventCode: "LONGLIFE",
+                eventCode: "DEFECTIVE",
                 start: date.at(i),
                 end: date.at(i+1)
             });
