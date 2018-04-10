b17a839c83bcb64dd875362fb36488b194095f14
VBurel <34450521+VBurel@users.noreply.github.com>
2018-04-10 14:15:42 -0700

    new

---
+++ training/lightbulb/src/analytics/SmartBulbLongLifeAlert.js
@@ -0,0 +1,27 @@
+var log = C3.logger("SmartBulbLongLifeAlert");
+
+var DURATION_THRESHOLD = 400
+
+function process(input) {
+    var data = input.bulbLife.data(), 
+    dates = input.bulbLife.dates(), 
+    startDate = input.source.startDate, 
+    endDate = dates[dates.length - 1],
+    durationOnMetricResult, durationOn;
+
+    duration = SmartBulb.evalMetric({id: input.source.id, 
+                                        expression: 'DurationOnInHours',
+                                        start: startDate,
+                                        end: endDate,
+                                        interval: 'DAY'});
+
+    durationOn = durationMetricResult[durationMetricResult.length - 1];
+    if (durationOn > DURATION_THRESHOLD) {
+        return SmartBulbEvent.make({
+            smartBulb: input.source,
+            eventCode: 'LONGLIFE',
+            start: date.at(i),
+            end: date.at(i+1)
+        });
+    }
+}
