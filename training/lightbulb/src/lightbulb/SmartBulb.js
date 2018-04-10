785e6bf4843f7ae9182e9137a578ca71f20298b4
Vikranth <vikranth363@gmail.com>
2018-04-09 17:04:11 -0700

    Day-5: Time Series and Metrics Workshop and App Logic Lab

--- training/lightbulb/src/lightbulb/SmartBulb.js
+++ training/lightbulb/src/lightbulb/SmartBulb.js
@@ -7,6 +7,17 @@
 * This product may be covered by one or more U.S. patents or pending patent applications.
 ~*/
 
+/*
+ * Returns the expected lumens of a light bulb based on wattage and bulbType
+ */
+function expectedLumens(wattage, bulbType) {
+  if(bulbType == 'LED')
+    return wattage * 84;
+  if(bulbType == 'INCAN')
+    return wattage * 14;
+  if(bulbType == 'CFL')
+    return wattage * 62;
+}
 
 /*
  * Returns the lifespan of a smart bulb in years.
@@ -22,4 +33,51 @@ function lifeSpanInYears(bulbId){
   conversionFactor = 1000*60*60*24*365;
   lifeSpanInYears = lifespan / conversionFactor;
   return lifeSpanInYears;
+}
+
+/*
+ * Returns the id of the smart bulb with the shortest life span.
+ */
+function shortestLifeSpanBulb(){
+  var bulbId, allLightBulbs, shortLifeBulbId, randomHighYears=99999, span;
+  allLightBulbs = SmartBulb.fetch({include:"id"}).objs;
+  for (var i = 0; i < allLightBulbs.length; i++) {
+    bulbId = allLightBulbs[i].id;
+    span = SmartBulb.lifeSpanInYears(bulbId);
+    if (span < randomHighYears){
+    	randomHighYears = span;
+      shortLifeBulbId = bulbId;
+    }
+  }
+  return shortLifeBulbId;
+}
+
+/*
+ * Returns the id of the smart bulb with the Longest life span.
+ */
+function longestLifeSpanBulb(){
+  var bulbId, allLightBulbs, longestLifeBulbId, randomLowYears=0, span;
+  allLightBulbs = SmartBulb.fetch({include:"id"}).objs;
+  for (var i = 0; i < allLightBulbs.length; i++) {
+    bulbId = allLightBulbs[i].id;
+    span = SmartBulb.lifeSpanInYears(bulbId);
+    if (span > randomLowYears){
+    	randomLowYears = span;
+      longestLifeBulbId = bulbId;
+    }
+  }
+  return longestLifeBulbId;
+}
+
+/*
+ * Returns all smart bulb's Average life span.
+ */
+function AverageLifeSpanBulb(){
+  var bulbId, allLightBulbs, averageLifeSpan=0, span=0;
+  allLightBulbs = SmartBulb.fetch({include:"id"}).objs;
+  for (var i = 0; i < allLightBulbs.length; i++) {
+    bulbId = allLightBulbs[i].id;
+    span += SmartBulb.lifeSpanInYears(bulbId);
+}
+  return span/allLightBulbs.length;
 }
