c43aa9bb6d27d7bf36972fc7fcffe0b133c08847
Harsh Pathania <akshatrana2007@gmail.com>
2018-04-11 15:42:07 -0700

    Update in file process function to execute fire analytics

--- training/lightbulb/src/SmartBulbOverheatAlert.js
+++ training/lightbulb/src/SmartBulbOverheatAlert.js
@@ -4,15 +4,15 @@ var Temperature_THRESHOLD = 80;
 
 function process(input) 
 {
-	var data = input.bulbHeat.data();
-	dates = input.bulbHeat.dates();
+	var data = input.bulbHeat.data(),
+	    dates = input.bulbHeat.dates();
 	for(var i=0;i<data.length;i++)
 		{
 		 if(data.at(i)>Temperature_THRESHOLD)
 			 {
 			  return SmartBulbEvent.make({
 				  smartBulb: input.source,
-				  eventCode: "Overheat",
+				  eventCode: "O",
 				  start: dates.at(i),
 				  end:dates.at(i+1)
 			  });
